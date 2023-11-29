3983f7099bc2bc5ed65a7a410f5a733d2179bdf9
Artem <artem.bezrukov.2008@mail.ru>
2023-11-29 20:43:39 +0400

    добавил цикл

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.body.innerHTML = '<h1>Hello from JavaScript!</h1>';
 console.log('Hello world!!!');
-for(let i = 0; i < 100; i++) {
-  console.log(`${i} Hello world!`)
+for (let i = 0; i < 101; i++) {
+    console.log(`${i} Hello world!`)
 }
